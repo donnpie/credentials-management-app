e21c3728b0d048e6dcd15b90d8f8929510d4a4af
Donn <donnpie@gmail.com>
2021-05-31 12:44:30 +0200

    Connect to mongoDb

---
+++ config/secrets.js
@@ -0,0 +1,5 @@
+const secrets = {
+    mongoUri: "mongodb+srv://Donn1:Donn123@cluster0.fb16n.mongodb.net/Cred_app?retryWrites=true&w=majority"
+}
+
+module.exports = secrets;
--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const db = require('./config/secrets').mongoUri;
 
 
 const app = express();
 
+//Connect to remote mongo
+mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true  })
+.then(() => console.log('MongoDb remote connected...'))
+.catch(err => console.log(err));
 
+//Middleware
+
+//Listen
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is now listening on port ${port}`));
